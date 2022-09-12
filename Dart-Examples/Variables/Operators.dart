5f6a915d8619c10da73d51aa4c36a850e9d6bebc
vitordbo <vitorduarte177@gmail.com>
2022-09-12 20:00:23 -0300

    Operators

---
+++ Dart-Examples/Variables/Operators.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  int number;
+
+  number = 1;
+  number++; //add 1 
+  //number--; // -1
+  ++number; // +1 before the result 
+
+  number+=2; // add 2 
+  //number-=2; // - 2
+
+  print(number);
+  print(++number);
+}
