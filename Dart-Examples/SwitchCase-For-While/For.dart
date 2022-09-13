2071dd9392a7b0b60325fbc10f0aea7788463c86
vitordbo <vitorduarte177@gmail.com>
2022-09-13 15:30:51 -0300

    for

---
+++ Dart-Examples/SwitchCase-For-While/For.dart
@@ -0,0 +1,19 @@
+void main() {
+  // for
+  // variable / condition / increment
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+
+  print('-------------------');
+
+  for (int x = 10; x >= 0; x--) {
+    print(x);
+  }
+
+  print('-------------------');
+
+  for (double a = 1; a <= 10; a += 0.5) {
+    print(a);
+  }
+}
