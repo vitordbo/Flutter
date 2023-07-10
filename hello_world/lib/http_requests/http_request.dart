f34972d23d12ca50abae4b8f1ab980d0f3b4e966
vitordbo <vitorduarte177@gmail.com>
2023-07-10 19:12:24 -0300

    json mapping

--- hello_world/lib/http_requests/http_request.dart
+++ hello_world/lib/http_requests/http_request.dart
@@ -3,14 +3,47 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future main() async {
-  final json = await fetch();
-  print(json);
+  final todo = await fetch();
+  print(todo.toJson());
 }
 
 // get from https://jsonplaceholder.typicode.com/
-Future<Map> fetch() async {
+Future<ToDo> fetch() async {
   Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
   var response = await http.get(url);
   var json = jsonDecode(response.body);
-  return json;
+  var todo = ToDo.fromJson(json);
+  return todo;
+}
+
+/* json response =
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  }
+*/
+
+// json mapping
+class ToDo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
+
+  ToDo(this.title, this.id, this.userId, this.completed);
+
+  factory ToDo.fromJson(Map json) {
+    return ToDo(json['title'], json['id'], json['userId'], json['completed']);
+  }
+
+  Map toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'completed': completed,
+      'userId': userId,
+    };
+  }
 }
