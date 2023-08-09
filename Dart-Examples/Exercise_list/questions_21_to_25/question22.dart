e1b3ee1e10776865dac37ebbcfa60e1a243e6a2d
vitordbo <vitorduarte177@gmail.com>
2023-08-09 14:42:39 -0300

    question 22

--- Dart-Examples/Exercise_list/questions_21_to_25/question22.dart
+++ Dart-Examples/Exercise_list/questions_21_to_25/question22.dart
@@ -1,3 +1,6 @@
+//Construa um programa que conte a quantidade de vezes que as palavras de uma frase aparecem.
+//Exemplo: “estude muito sempre sempre estude” Resultado: estude=2 muito=1 sempre=2
+
 import 'dart:io';
 
 void main() {
